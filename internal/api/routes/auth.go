a6a7ef496c7d8403ebc82ecc1d32d38183b73b7a
Taofeek Abdulazeez <102738186+Taofeekabdulazeez@users.noreply.github.com>
2025-09-30 18:09:39 +0100

    refactor: remove unused email verification route and service dependency

--- internal/api/routes/auth.go
+++ internal/api/routes/auth.go
@@ -3,7 +3,6 @@ package routes
 import (
 	"todo-api-go/internal/api/handler"
 	"todo-api-go/internal/api/middleware"
-	"todo-api-go/internal/core/service"
 
 	"github.com/gin-gonic/gin"
 )
@@ -19,17 +18,5 @@ func RegisterAuthRoutes(router *gin.RouterGroup) {
 		authRoutes.GET("/user", authHandler.GetAuthUser)
 		authRoutes.PATCH("/user", middleware.RequireAuth(), authHandler.UpdateUser)
 		authRoutes.POST("/logout", authHandler.Logout)
-		authRoutes.GET("/sendMail", func(ctx *gin.Context) {
-			emailS := &service.MailService{}
-			if err := emailS.SendVerificationEmail("taofeekabdulazeeztaiwo@gmail.com", "hello"); err != nil {
-				ctx.AbortWithStatusJSON(500, gin.H{
-					"success": false,
-					"message": "Email sending failed",
-					"error":   err.Error(),
-				})
-				return
-			}
-			ctx.JSON(200, gin.H{"success": true, "message": "Email sent successfully"})
-		})
 	}
 }
